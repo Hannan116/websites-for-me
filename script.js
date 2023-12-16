e3c4c6d59d8d3ecf3d3f727e946925d73c5c4aba
Hamnnan116 <132570825+Hamnnan116@users.noreply.github.com>
2023-12-16 09:45:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const button = document.querySelector("#gmail");
+button.addEventListener("click", function () {
+  window.open("https://mail.google.com/mail/u/1/?ogbl#inbox", "_blank");
+});
+document.querySelector("#youtube").addEventListener("click", function () {
+  window.open("https://www.youtube.com/", "_blank");
+});
+document.querySelector("#w3school").addEventListener("click", function () {
+  window.open("https://www.w3schools.com/", "_blank");
+});
+document.querySelector("#googlebard").addEventListener("click", function () {
+  window.open("https://bard.google.com/", "_blank");
+});
+document.querySelector("#cricinfo").addEventListener("click", function () {
+  window.open("https://www.espncricinfo.com/", "_blank");
+});
+document.querySelector("#prothomalo").addEventListener("click", function () {
+  window.open("https://www.prothomalo.com/", "_blank");
+});
+document.querySelector("#dailystar").addEventListener("click", function () {
+  window.open("https://www.thedailystar.net/", "_blank");
+});
+document.querySelector("#manabzamin").addEventListener("click", function () {
+  window.open("https://mzamin.com/", "_blank");
+});
+document.querySelector("#gtcl").addEventListener("click", function () {
+  window.open("https://gtcl.org.bd/", "_blank");
+});
+document.querySelector("#pb").addEventListener("click", function () {
+  window.open("http://petrobangla.org.bd/", "_blank");
+});
+document.querySelector("#emrd").addEventListener("click", function () {
+  window.open("https://emrd.gov.bd/", "_blank");
+});
+document.querySelector("#enothi").addEventListener("click", function () {
+  window.open("https://nothi.gov.bd/login", "_blank");
+});
+document.querySelector("#converter").addEventListener("click", function () {
+  window.open(
+    "https://hamnnan116.github.io/MyNewConverter/Converter.html",
+    "_blank"
+  );
+});
+document.querySelector("#fileconverter").addEventListener("click", function () {
+  window.open("https://cloudconvert.com/", "_blank");
+});
+document.querySelector("#drawdiagram").addEventListener("click", function () {
+  window.open("https://www.drawio.com/", "_blank");
+});
+document.querySelector("#printfriendly").addEventListener("click", function () {
+  window.open("https://www.printfriendly.com/", "_blank");
+});
+document.querySelector("#pdfeditor").addEventListener("click", function () {
+  window.open("https://www.sejda.com/pdf-editor", "_blank");
+});
+document.querySelector("#aiyou").addEventListener("click", function () {
+  window.open("https://you.com/", "_blank");
+});
+document.querySelector("#sofascore").addEventListener("click", function () {
+  window.open("https://www.sofascore.com/", "_blank");
+});
